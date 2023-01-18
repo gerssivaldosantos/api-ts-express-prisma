0b814912e7e41ec6b7f858cbd65cd10527f5a19f
gerssivaldo <canalandows@gmail.com>
2023-01-17 22:17:04 -0300

    chore: modify basic structure

--- src/useCases/helloWorld/module.ts
+++
@@ -1,7 +0,0 @@
-import { HelloWorldController } from "./controller";
-import { HelloWorldService } from "./service";
-
-const service = new HelloWorldService()
-const helloWorldController = new HelloWorldController(service)
-
-export { helloWorldController }
